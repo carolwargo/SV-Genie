a0f7e0efda875e6cb8aa45b9abf37854492032c1
Carol Wargo <Carolwargo@gmail.com>
2023-06-06 09:31:04 -0400

    update

--- Assets/lib/TEXT.js
+++ Assets/lib/TEXT.js
@@ -1,1 +1,16 @@
-// Exports `TEXT` class
+class Text {
+    constructor(content = '') {
+      this.content = content;
+    }
+  
+    setContent(content) {
+      this.content = content;
+    }
+  
+    render() {
+      return this.content;
+    }
+  }
+  
+  module.exports = Text;
+  
--- Assets/lib/TEXT.test.js
+++ Assets/lib/TEXT.test.js
@@ -1,1 +1,19 @@
-// Jest tests for TEXT
+const Text = require('../lib/text');
+
+describe('Text', () => {
+  test('should render empty text', () => {
+    const text = new Text();
+    expect(text.render()).toBe('');
+  });
+
+  test('should render provided text', () => {
+    const text = new Text('Hello, world!');
+    expect(text.render()).toBe('Hello, world!');
+  });
+
+  test('should set content correctly', () => {
+    const text = new Text();
+    text.setContent('Hello, world!');
+    expect(text.render()).toBe('Hello, world!');
+  });
+});
